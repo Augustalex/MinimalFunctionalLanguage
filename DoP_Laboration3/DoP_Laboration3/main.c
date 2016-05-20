43f4bc37ef2607a2cf6cf2add6a208f10630e1cd
August Alexandersson <augustalexandersson@gmail.com>
2016-05-20 16:57:27 +0200

    sfesfesafjyt

---
+++ DoP_Laboration3/DoP_Laboration3/main.c
@@ -0,0 +1,30 @@
+#include "parser.h"
+#include "print.h"
+#include "symtab.h"
+#include "exp.h"
+#include "exception.h"
+#include "value.h"
+
+
+main()
+{
+	scannerADT scanner;
+	expADT exp;
+	string line;
+	InitVariableTable();
+	scanner = NewScanner();
+	SetScannerSpaceOption(scanner, IgnoreSpaces);
+	while (TRUE) {
+		try {
+			printf("=> ");
+			line = GetLine();
+			if (StringEqual(line, "quit")) exit(0);
+			SetScannerString(scanner, line);
+			exp = ParseExp(scanner);
+			valueADT value = EvalExp(exp);
+			PrintValue(value);
+			except(ErrorException)
+				printf("Error: %s\n", (string)GetExceptionValue());
+		} endtry
+	}
+}
